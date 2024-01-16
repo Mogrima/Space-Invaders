0a8666ff4993be41e5ebc2f810617eba070de004
Mogrima <rebel.rs@yandex.ru>
2024-01-17 01:36:39 +0500

    added files to project

---
+++ srcipt.js
@@ -0,0 +1,8 @@
+"use strict";
+
+window.addEventListener('load', function() {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 600;
+    canvas.height = 800;
+});
