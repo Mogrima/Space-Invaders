b7b30bebbc36647777c8b4239f8f92d4f4b45f8b
Mogrima <rebel.rs@yandex.ru>
2024-01-17 02:04:54 +0500

    animation function has been created. Added an instance of the Game class and ran the render method inside the function animate

--- srcipt.js
+++ srcipt.js
@@ -1,8 +1,21 @@
 "use strict";
 
+import { Game } from "./src/Game.js";
+
 window.addEventListener('load', function() {
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
     canvas.width = 600;
-    canvas.height = 800;
+    canvas.height = 700;
+
+    const game = new Game(canvas);
+    
+
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.render(ctx);
+        requestAnimationFrame(animate);
+    }
+
+    animate();
 });
