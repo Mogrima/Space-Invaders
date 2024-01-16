f587f0aee0b0369f6e75889535424667798816f0
Mogrima <rebel.rs@yandex.ru>
2024-01-17 03:05:55 +0500

    added basic properties and methods for the class Projectile. The class will use the object pool pattern in its implementation

--- src/Classes/Projectile.js
+++ src/Classes/Projectile.js
@@ -1,2 +1,28 @@
-class Projectile {
+export class Projectile {
+    constructor() {
+        this.width = 4;
+        this.height = 20;
+        this.x = 0;
+        this.y = 0;
+        this.speed = 20;
+        this.free = true;
+    }
+
+    draw(context) {
+        if (!this.free) {
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+
+    update() {
+        if (!this.free) {
+            this.y -= this.speed;
+        }
+    }
+    start() {
+        this.free = false;
+    }
+    reset() {
+        this.free = true;
+    }
 }
