c420d1daf07bcdfc53c72bbed895d8962428027c
Mogrima <rebel.rs@yandex.ru>
2024-01-17 02:02:43 +0500

    added basic properties and methods for the class Game. Instance of the player class has been created

---
+++ src/Game.js
@@ -0,0 +1,15 @@
+import { Player } from "./Classes/Player.js";
+
+export class Game {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.player = new Player(this);
+
+    }
+    render(context) {
+        this.player.draw(context);
+        this.player.update();
+    }
+}
