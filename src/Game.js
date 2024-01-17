aeacc4bb518023965965aedbf61c4a498ee98032
Mogrima <rebel.rs@yandex.ru>
2024-01-17 07:59:43 +0500

    add method checkCollision

--- src/Game.js
+++ src/Game.js
@@ -48,10 +48,20 @@ export class Game {
             this.projectilesPool.push(new Projectile());
         }
     }
+    
     // get free projectile object from the pool
     getProjectile() {
         for (let i = 0; i < this.projectilesPool.length; i++) {
             if (this.projectilesPool[i].free) return this.projectilesPool[i];
         }
+    }
+
+    
+    checkCollision(rect1, rect2) {
+        return (
+            rect1.x < rect2.x + rect2.width &&
+            rect2.x < rect1.x + rect1.width &&
+            rect1.y < rect2.y + rect2.height &&
+            rect2.y < rect1.y + rect1.height);
     }
 }
