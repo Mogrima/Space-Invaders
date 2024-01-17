345ec54cf02810cce6d3651b5df5f4d1b8eb3176
Mogrima <rebel.rs@yandex.ru>
2024-01-17 06:04:26 +0500

    added methods for creating projectiles and receiving them for the player

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,5 @@
 import { Player } from "./Classes/Player.js";
+import { Projectile } from "./Classes/Projectile.js";
 
 export class Game {
     constructor(canvas) {
@@ -8,8 +9,13 @@ export class Game {
         this.keys = [];
         this.player = new Player(this);
 
+        this.projectilesPool = [];
+        this.numberOfProjectiles = 10;
+        this.createProjectiles();
+
         window.addEventListener('keydown', e => {
             if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
+            if (e.key === '1') this.player.shoot();
         });
         window.addEventListener('keyup', e => {
             const index = this.keys.indexOf(e.key);
@@ -20,5 +26,21 @@ export class Game {
     render(context) {
         this.player.draw(context);
         this.player.update();
+        this.projectilesPool.forEach(projectile => {
+            projectile.update();
+            projectile.draw(context);
+        })
+    }
+
+    createProjectiles() {
+        for (let i = 0; i < this.numberOfProjectiles; i++) {
+            this.projectilesPool.push(new Projectile());
+        }
+    }
+    // get free projectile object from the pool
+    getProjectile() {
+        for (let i = 0; i < this.projectilesPool.length; i++) {
+            if (this.projectilesPool[i].free) return this.projectilesPool[i];
+        }
     }
 }
