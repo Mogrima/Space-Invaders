706bf53b6f5dfef4c2e4224718940ec0f4638400
Mogrima <rebel.rs@yandex.ru>
2024-01-27 22:23:23 +0500

    added sprite new enemy and created class Rhinomorph

---
+++ src/Classes/Rhinomorph.js
@@ -0,0 +1,18 @@
+import { Enemy } from "./Enemy.js";
+
+export class Rhinomorph extends Enemy {
+    constructor(game, positionX, positionY) {
+        super(game, positionX, positionY);
+        this.image = document.getElementById('rhinomorph');
+        this.frameX = 0;
+        this.frameY = Math.floor(Math.random() * 4);
+        this.maxFrame = 5;
+        this.lives = 4;
+        this.maxLives = this.lives;
+    }
+
+    hit(damage) {
+        this.lives -= damage;
+        this.frameX = this.maxLives - Math.floor(this.lives);
+    }
+}
