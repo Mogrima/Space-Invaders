cb7ec0562bcf603ff0265937e4eeab8a5ef8ea9f
Mogrima <rebel.rs@yandex.ru>
2024-01-28 22:15:15 +0500

    Fixed life check to be less than 0 so that the boss does not remain alive if he has less than one life left, but more than 0 when colliding with a laser, because azar has damage in decimal values

--- src/Classes/Enemies/Boss.js
+++ src/Classes/Enemies/Boss.js
@@ -19,23 +19,23 @@ export class Boss {
     draw(context) {
         context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height,
                 this.width, this.height, this.x, this.y, this.width, this.height);
-        if (this.lives > 0) {
+        if (this.lives >= 1) {
             context.save();
             context.textAlign = 'center';
             context.shadowOffsetX = 3;
             context.shadowOffsetY = 3;
             context.shadowColor = 'black';
-            context.fillText(this.lives, this.x + this.width * 0.5, this.y + 50);
+            context.fillText(Math.floor(this.lives), this.x + this.width * 0.5, this.y + 50);
             context.restore();
         }
     }
 
     update() {
         this.speedY = 0;
-        if (this.game.spriteUpdate && this.lives > 0) this.frameX = 0;
+        if (this.game.spriteUpdate && this.lives >= 1) this.frameX = 0;
         if (this.y < 0) this.y += 10;
         if (this.x < 0 || this.x > this.game.width - this.width
-            && this.lives > 0) {
+            && this.lives >= 1) {
             this.speedX *= -1;
             this.speedY = this.height * 0.5;
         }
@@ -44,13 +44,13 @@ export class Boss {
 
         this.game.projectilesPool.forEach(projectile => {
             if (!projectile.free && this.game.checkCollision(this, projectile)
-                && this.lives > 0 && this.y >= 0) {
+                && this.lives >= 1 && this.y >= 0) {
                 this.hit(1);
                 projectile.reset();
             }
         });
 
-        if (this.game.checkCollision(this, this.game.player) && this.lives > 0) {
+        if (this.game.checkCollision(this, this.game.player) && this.lives >= 1) {
             this.game.gameOver = true;
             this.lives = 0;
         }
@@ -70,6 +70,6 @@ export class Boss {
 
     hit(damage) {
         this.lives -= damage;
-        if (this.lives > 0) this.frameX = 1;
+        if (this.lives >= 1) this.frameX = 1;
     }
 }
